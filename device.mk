62c57c43389c37ca8c2d1fd6817758a74e3ed686
Bruno Martins <bgcngm@gmail.com>
2021-06-05 15:50:10 +0100

    thyme: overlay-lineage: Declare support for wireless charging
    Change-Id: Iba6a8b357779a3bb2134f2fc0878819339615239

--- device.mk
+++ device.mk
@@ -39,7 +39,8 @@ TARGET_HAS_UDFPS := true
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-lineage
 
 # QDCM
 PRODUCT_COPY_FILES += \
