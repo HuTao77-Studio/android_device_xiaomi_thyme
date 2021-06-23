73543a9a9db744adb48cd8d091bb2cbb60703b6c
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-06-23 19:36:07 +0200

    thyme: Add libinit for variants handling
    Change-Id: Ia990f08d046192a42c0d951a2ce8d06b8794f6f0
    Co-Authored-By: Yumi Yukimura <me.cafebabe@gmail.com>

---
+++ libinit/init_xiaomi_thyme.cpp
@@ -0,0 +1,28 @@
+/*
+ * Copyright (C) 2021 The LineageOS Project
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <libinit_dalvik_heap.h>
+#include <libinit_variant.h>
+
+#include "vendor_init.h"
+
+static const variant_info_t thyme_info = {
+    .hwc_value = "",
+    .sku_value = "",
+
+    .brand = "Xiaomi",
+    .device = "thyme",
+    .marketname = "Mi 10S",
+    .model = "M2102J2SC",
+    .build_fingerprint = "Xiaomi/thyme/thyme:13/RKQ1.211001.001/V816.0.4.0.TGACNXM:user/release-keys",
+
+    .nfc = true,
+};
+
+void vendor_load_properties() {
+    set_variant_props(thyme_info);
+    set_dalvik_heap();
+}
