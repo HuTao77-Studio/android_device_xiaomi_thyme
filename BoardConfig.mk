a8d57d559260f2d4dbc9268a95539e36b259a2d8
Aaron Kling <webgeek1234@gmail.com>
2023-09-12 16:08:51 -0500

    thyme: Re-enable broken dup rules check
    All failures of this have been resolved
    
    Change-Id: I56a923ac90b98e63c093c8cb65cb1059dde57c49

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -9,8 +9,6 @@ DEVICE_PATH := device/xiaomi/thyme
 # Inherit from sm8250-common
 include device/xiaomi/sm8250-common/BoardConfigCommon.mk
 
-BUILD_BROKEN_DUP_RULES := true
-
 # Board
 TARGET_BOARD_INFO_FILE := $(DEVICE_PATH)/board-info.txt
 
