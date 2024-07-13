a61a6f77bd2ee75ca9d8b8b8cae8dc9fe61af6fb
Fuutao <fuutaoao@gmail.com>
2024-07-13 20:24:35 +0800

    thyme: Import device-specific stock overlays
     * From thyme-user 13 RKQ1.211001.001 V816.0.4.0.TGACNXM
    
    Signed-off-by: Fuutao <fuutaoao@gmail.com>
    Co-Authored-By: LynnrinChan <lynnrin@lynnrin.moe>

--- device.mk
+++ device.mk
@@ -22,6 +22,10 @@ PRODUCT_COPY_FILES += \
 TARGET_SCREEN_HEIGHT := 2340
 TARGET_SCREEN_WIDTH := 1080
 
+# Overlays
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay
+
 # QDCM
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(LOCAL_PATH)/qdcm/,$(TARGET_COPY_OUT_VENDOR)/etc)
