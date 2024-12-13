1193f6f89645fbf07f8bd3ab4f64e5005e11fc5a
Fuutao <fuutaoao@gmail.com>
2024-12-14 00:04:57 +0800

    thyme: Crdroid bringup

--- lineage_thyme.mk
+++ lineage_thyme.mk
@@ -8,7 +8,7 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Lineage stuff.
+# Inherit some common Crdroid stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Inherit from thyme device
